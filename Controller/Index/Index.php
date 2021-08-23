e7de2be1adb6c597458f79fc152be3be72cf262b
rafael <elrafafa1992@hotmail.com>
2021-08-23 18:05:37 -0500

    funcion f 2

--- Controller/Index/Index.php
+++ Controller/Index/Index.php
@@ -1,7 +1,10 @@
 <?php
+
 namespace Omnipro\Reto1\Controller\Index;
 
 use Omnipro\Reto1\Model\Reto1Factory;
+use \Magento\User\Model\UserFactory;
+//use Magento\Framework\App\Filesystem\DirectoryList;
 
 class Index implements \Magento\Framework\App\Action\HttpGetActionInterface
 {
@@ -11,22 +14,29 @@ class Index implements \Magento\Framework\App\Action\HttpGetActionInterface
     protected $_pageFactory;
     protected $_Reto1Factory;
     protected $_request;
+    protected $_UserFactory;
+
+    //para subir imagen
+    protected $_fileUploaderFactory;
 
     /**
      * @param \Magento\Framework\App\Action\Context $context
      */
     public function __construct(
-        
+
         \Magento\Framework\App\RequestInterface $request,
         \Magento\Framework\View\Result\PageFactory $pageFactory,
-        Reto1Factory $Reto1F
-    )
-    {
-        
+        Reto1Factory $Reto1F,
+        UserFactory $UserFactory
+    
+        //\Magento\MediaStorage\Model\File\UploaderFactory $fileUploaderFactory
+    ) {
+
         $this->_pageFactory = $pageFactory;
         $this->_Reto1Factory = $Reto1F;
         $this->_request = $request;
-        
+        $this->_UserFactory=$UserFactory;
+        //$this->_fileUploaderFactory = $fileUploaderFactory;
     }
     /**
      * View page action
@@ -35,10 +45,19 @@ class Index implements \Magento\Framework\App\Action\HttpGetActionInterface
      */
     public function execute()
     {
-        $getp=$this->_request->getParams();
-         //crar entrada en la tabla useropinion
-         $pos = $this->_Reto1Factory->create(); //instancia user opinion
         
+        $getp = $this->_request->getParams();
+        //crar entrada en la tabla useropinion
+        $pos = $this->_Reto1Factory->create(); //instancia user opinion
+        $userF=$this->_UserFactory->create();
+        $userF=$userF->getCollection()->addFieldToFilter('email', array('like' => '% elrafafa'));
+        
+        foreach($userF as $item){
+			echo "<pre>";
+			print_r($item);
+			echo "</pre>";
+		}
+
         if (!empty($getp)) {
             // Retrieve your form data
             $title  = $getp['title'];
@@ -46,14 +65,27 @@ class Index implements \Magento\Framework\App\Action\HttpGetActionInterface
             $email = $getp['email'];
             $file   = $getp['file'];
 
-           
+            //calidar correo administrador 
+            
+            
+
             //manera 1 de enviar datos
             $pos->setData('title', $title);
             $pos->setData('content', $content);
             $pos->setData('image_path', $file);
             $pos->setData('email', $email);
             $pos->save();
+            /*
+            $uploader = $this->_fileUploaderFactory->create(['fileId' => $file]);
+            $uploader->setAllowedExtensions(['jpg', 'jpeg', 'gif', 'png']);
+            $uploader->setAllowRenameFiles(false);
+            $uploader->setFilesDispersion(false);
+            $path = $this->_filesystem->getDirectoryRead(DirectoryList::MEDIA)->getAbsolutePath('imagenes/');
+            $uploader->save($path);*/
+
+            
         }
         return $this->_pageFactory->create();
     }
+
 }
--- ViewModel/Reto1.php
+++ ViewModel/Reto1.php
@@ -1,20 +1,33 @@
 <?php
 namespace Omnipro\Reto1\ViewModel;
 
+use Omnipro\Reto1\Model\Reto1Factory;
+//use \Magento\User\Model\UserFactory;
 class Reto1 implements \Magento\Framework\View\Element\Block\ArgumentInterface
 {
-        protected $curl;
-
+        //protected $curl;
+        protected $_Reto1Factory;
+        //protected $_UserFactory;
         public function __construct(
-            \Magento\Framework\HTTP\Client\Curl $curl
+            //\Magento\Framework\HTTP\Client\Curl $curl,
+            Reto1Factory $Reto1F
+            //UserFactory $UserFactory
         )
         {
-            $this->curl = $curl;
+            //$this->curl = $curl;
+            $this->_Reto1Factory = $Reto1F;
+            //$this->_UserFactory=$UserFactory;
+
         }
     
-        public function getCharacters(){
-            $this->curl->get('https://rickandmortyapi.com/api/character/');
+        public function getDat(){
+            $colections=$this->_Reto1Factory->create();
             //return $this->curl->getBody();
-            return json_decode($this->curl->getBody(), true);
+            return $colections->getCollection();
         }
+
+        /*public function getUser(){
+            $userF=$this->_UserFactory->create();
+            return $userF->getCollection();
+        }*/
  }
